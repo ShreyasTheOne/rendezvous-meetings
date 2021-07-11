f2444c6e197db71d85e077bee8ef59969fa8afed
Shreyas Dodamani <shreyurd@gmail.com>
2021-07-11 16:01:36 +0530

    Create configuration stencils

--- backend/rendezvous_backend/settings.py
+++ backend/rendezvous_backend/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG')
+DEBUG = True if os.environ.get('DJANGO_DEBUG') == "TRUE" else False
 
 ALLOWED_HOSTS = []
 
